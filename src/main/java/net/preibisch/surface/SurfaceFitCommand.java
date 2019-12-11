590dedff4384e980e63c4edec8e95bbd5b5d92f9
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-12-11 16:11:42 -0500

    Tossing together a Command to sanity check 1-to-1 behavior against amira

---
+++ src/main/java/net/preibisch/surface/SurfaceFitCommand.java
@@ -0,0 +1,55 @@
+package net.preibisch.surface;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.FolderOpener;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.real.FloatType;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import static net.preibisch.surface.Test.process2;
+import static net.preibisch.surface.Test.renderDepthMap;
+
+@Plugin(type = Command.class)
+public class SurfaceFitCommand implements Command {
+    //@Parameter
+    private String inputDirectory = "/home/kharrington/Data/SEMA/Z1217_19m/Sec04/flatten/tmp-flattening-level200/resampled/";
+
+    //@Parameter
+    private String outputDirectory;
+
+    //@Parameter
+    private long originalDimX;
+
+    //@Parameter
+    private long originalDimZ;
+
+    @Override
+    public void run() {
+        // Open image sequence from input directory
+        ImagePlus imp = FolderOpener.open(inputDirectory, " file=tif");
+        // Reslice image
+        IJ.run(imp, "Reslice [/]...", "output=1.000 start=Top avoid");
+
+        Img img = ImageJFunctions.wrap(imp);
+        
+		final Img<IntType> surface = process2( img, 5, 40, 20 );
+
+		final Img<FloatType> rendererSurface = img.factory().create( img );
+
+		renderDepthMap( rendererSurface, surface );
+
+		//Gauss3.gauss( 0.7, Views.extendMirrorSingle( rendererSurface ), rendererSurface );
+
+		Util.getImagePlusInstance( img ).show();
+		Util.getImagePlusInstance( surface ).show();
+		Util.getImagePlusInstance( rendererSurface ).show();
+
+		//IJ.run(imp, "Save to HDF5 File (new or replace)...", "save=/home/kharrington/Data/SEMA/Z1217_19m/Sec04/flatten/tmp-flattening-level200/heightSurf/test.h5");
+    }
+}
